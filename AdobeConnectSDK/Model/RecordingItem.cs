72c04ff5a8f35714c4cf78bc0a19514e36f163d1
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2021-12-11 21:39:32 +0330

    Update 1.0.1

---
+++ AdobeConnectSDK/Model/RecordingItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace AdobeConnectSDK.Model
+{
+    /// <summary>
+    /// Recording information.
+    /// </summary>
+    [Serializable]
+    [XmlRoot("recording")]
+    public class RecordingItem : XmlDateTimeBase
+    {
+        [XmlElement("name")]
+        public string MeetingName;
+
+        [XmlElement("url-path")]
+        public string UrlPath;
+
+        [XmlElement("is-seminar")]
+        public bool IsSeminar;
+    }
+}
